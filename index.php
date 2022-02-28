015c8347a7c5d10ddc1451f7db17ab05d68b9151
thais <thaiscoellen@gmail.com>
2022-02-28 20:26:11 +0100

    inclusao Bootstrap na index

--- index.php
+++ index.php
@@ -21,16 +21,44 @@ if($_POST){
 }
 
 ?>
-<h1>Calculadora da Thaís</h1>
 
-<form method="post" action="index.php">
-    Selecione a operação
-    <select name="operacao" required>
-        <option value="">Selecione um opção</option>
-        <option value="soma">Somar</option>
-        <option value="subtracao">Subtração</option>
-        <option value="multiplicacao">Multiplicação</option>
-        <option value="divisao">Divisão</option>
-    </select>
-    <input type="submit">
-</form>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Calculadora da Thaís</title>
+  </head>
+  <body>
+    <h1>Calculadora da Thaís</h1>
+    <form method="post" action="index.php">
+        <div class="row g-3 align-items-center">
+            <div class="col-auto">
+                <label class="col-form-label">Selecione a operação</label>
+            </div>
+            <div class="col-3">
+                <select class="form-control" name="operacao" required>
+                    <option value="">Selecione um opção</option>
+                    <option value="soma">Somar</option>
+                    <option value="subtracao">Subtração</option>
+                    <option value="multiplicacao">Multiplicação</option>
+                    <option value="divisao">Divisão</option>
+                </select>
+            </div>
+            <div class="col-3">
+                <button type="submit" class="btn btn-success">Calcular</button>
+            </div>
+        </div>
+    </form>
+  </body>
+</html>
+
+
+
+
+
